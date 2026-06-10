d37ec464e2e060533f061e694398a210b65d6000
Krishna Kumar Chaurasia <krishnakumarchaurasia6@gmail.com>
2026-06-10 12:55:50 +0530

    Updated files

---
+++ JavaScript/Day_01.js
@@ -0,0 +1,12 @@
+const a= 10
+const b= 20
+const c= 30
+const d= 40
+
+const sum = a + b //30
+const sum2 = c + d //70
+
+console.log(sum>50 && sum2>50) // false && true => false
+console.log(sum<50 || sum2<50) // true || false => true
+console.log(sum>100 || sum2>100) // false || false => false
+
---
+++ JavaScript/If_condition.js
@@ -0,0 +1,50 @@
+// 1st condition
+const a= 85
+
+if (a>=90){
+    console.log("A")
+}
+else if  (a>=75){
+    console.log("B")
+}
+else if(a>=50){
+    console.log("C")
+}
+else{
+    console.log("FAIL")
+}
+
+
+// 2nd condition
+const code =1
+
+if(code===1){
+    console.log("mobile")
+}
+else if(code===2){
+    console.log("laptop")
+}
+else if(code===3){
+    console.log("tablet")
+}
+else if(code===4){
+    console.log("desktop")
+}
+else if(code===5){
+    console.log("smartwatch")
+}
+else if (code==="1"){
+    console.log("swift")
+}
+else if (code==="2"){
+    console.log("Frauri")
+}   
+else if (code==="3"){
+    console.log("audi")
+}
+else if (code==="4"){
+    console.log("BMW")
+}
+else if(code==="5"){
+    console.log("TATA neu")
+}
